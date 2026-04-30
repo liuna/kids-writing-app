176dc1a0b8a9f2c47f377faf8509d88e449d36f3
遥雪 <liuna.liuna@alipay.com>
2026-04-30 18:48:56 +0800

    修改路径问题

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-const baseName = import.meta.env.BASE_URL
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename={baseName}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
